f167216f62520e305ff9cb51b212236e75d9f600
RunqiZhao <69561314+RunqiZhao@users.noreply.github.com>
2020-11-10 18:18:01 -0500

    before and after

--- cleaning.R
+++ cleaning.R
@@ -43,3 +43,34 @@ for (i in 1:dim(sp500)[1]){
     sp500$pub[i] <-0
   }
 }
+
+sp500$pct <- NA
+for (i in 2:dim(sp500)[1]){
+  sp500$pct[i] <- (sp500$Adj.Close[i] - sp500$Adj.Close[(i-1)])/sp500$Adj.Close[(i-1)]
+}
+
+sp500 <- sp500 %>% select("Date","Close","Adj.Close","ym","pub","pct")
+
+sp500$BA <- NA
+sp500$group <- NA
+for (i in 5:dim(sp500)[1]){
+  if(sp500$pub[i] == 1){
+    sp500$BA[(i-5):(i-1)] <- "Before"
+    sp500$BA[i:(i+4)] <- "After"
+    sp500$group[(i-5):(i+4)] <- sp500$Date[i]
+  }
+}
+
+dt <- na.omit(sp500)
+
+before <- dt %>% filter(BA == "Before")
+after <- dt %>% filter(BA == "After")
+
+# dt2 <- before %>% group_by(group) %>% mean(pct)
+dt2 <- ddply(before,.(group),function(sub){data.frame(pct.mean = mean(sub$pct))})
+bb <- dt2$pct.mean
+hist(bb)
+
+dt3 <- ddply(after,.(group),function(sub){data.frame(pct.mean = mean(sub$pct))})
+aa <- dt3$pct.mean
+hist(aa)
